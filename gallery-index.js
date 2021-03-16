ae4880d533be048ad2b8a31d82ede69a7aa5b89f
Olena Kozakova <helen_kozakova@ukr.net>
2021-03-16 17:11:17 +0200

    фиксит баги разметки галереи

--- gallery-index.js
+++ gallery-index.js
@@ -1,28 +1,31 @@
 import galleryItems from "./gallery-items.js";
 
+/*Создание и рендер разметки по массиву данных и предоставленному шаблону.*/
 const galleryContainerRef = document.querySelector(".js-gallery");
 const cardImagesRef = createCardsGallery(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML("beforeend", cardImagesRef);
 
 function createCardsGallery(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
-            return
-            `<li class="gallery__item">
+            return`<li class="gallery__item">
     <a
         class="gallery__link"
-        href="${original}"
-    >
-    <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-    />
+        href = "${original}"
+>
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
     </a>
-</li>`}).join("");
+</li>`
+    }).join('')
 }
 
-galleryContainerRef.insertAdjacentHTML("beforeend", cardImagesRef);
+
 
 //const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
 //const lightboxCloseRef = document.querySelector('.close-lightbox');
